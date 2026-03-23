896b80387c45bdb5392ca05f3f49a59781feec2d
this-is-rasank <rasankverma.rv@gmail.com>
2026-03-23 19:43:03 +0530

    Constructor changes + Formatting

--- OperatorOverloading/OO.cpp
+++ OperatorOverloading/OO.cpp
@@ -1,28 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Rational
-{   
-    public:
-        int num,den;
-        Rational(int num,int den)
-        {
-            this->num=num;
-            this->den=den;
-        }
-        Rational operator +(Rational r)
-        {
-            int num=this->num*r.den + this->den*r.num;
-            int den=this->den*r.den;
-            return Rational(num, den);
-        }
+{
+public:
+    int num, den;
+    Rational(int num, int den);
+    Rational operator+(Rational r);
 };
+
+Rational::Rational(int num = 0, int den = 1) : num(num), den(den) {};
+
+Rational Rational::operator+(Rational r)
+{
+    Rational temp;
+    temp.num = this->num * r.den + this->den * r.num;
+    temp.den = this->den * r.den;
+    return temp;
+}
 
 int main()
 {
-    Rational r1(1,2);
-    Rational r2(1,3);
-    Rational r3=r1+r2;
-    cout<<r3.num<<"/"<<r3.den;
+    Rational r1(1, 2);
+    Rational r2(1, 3);
+    Rational r3 = r1 + r2;
+    cout << r3.num << "/" << r3.den;
     return 0;
 }
