9d705f7a9b415e2d460cb517b6ec11886b16b9fd
this-is-rasank <rasankverma.rv@gmail.com>
2026-03-23 20:47:02 +0530

    type conversion using operator overloading

--- OperatorOverloading/OO.cpp
+++ OperatorOverloading/OO.cpp
@@ -11,7 +11,15 @@ public:
     {
         cout << whole << " " << num << "/" << den;
     }
+    // type conversion using operator overloading
+    operator string()
+    {
+        return str(*this);
+    };
     Rational operator+(Rational r);
+
+private:
+    string str(Rational r);
 };
 
 Rational::Rational(int num = 0, int den = 1, int whole = 0) : num(num), den(den), whole(whole) {};
@@ -43,11 +51,19 @@ Rational Rational::operator+(Rational r)
     return temp;
 }
 
+string Rational::str(Rational r)
+{
+    return to_string(r.whole) + " " + to_string(r.num) + "/" + to_string(r.den);
+}
+
 int main()
 {
     Rational r1(3, 2);
     Rational r2(1, 3);
     Rational r3 = (r1 + r2).reduce();
     r3.print();
+    // Type conversion to string
+    cout << endl
+         << "Rational as string: " << (string)r3 << endl;
     return 0;
 }
